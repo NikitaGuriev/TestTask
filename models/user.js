9c8b34d83d2ec3121e685cdfe8b11586ff8993e3
NikitaGuriev <stig.guriev@gmail.com>
2022-04-19 12:20:18 +0300

    Удаление лишнего

--- models/user.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model} = require('mongoose');
-
-const SCHEMA = new Schema({
-  login: {type: String, required: true, unique: true},
-  password: {type: String, requried: true}
-});
-
-module.exports = model('User', SCHEMA);
--- react-kh/public/js/pwa_app.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load', async () => {
-  if ('serviceWorker' in navigator) {
-    try {
-      const REG = await navigator.serviceWorker.register('./sw.js');
-    } catch (err) {
-      console.log('Service worker register fail!', err);
-    }
-  }
-});
